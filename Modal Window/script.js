f58d931041784ddae9040eff51483e18bd6f9c7b
Anthony Campos <Anthonyck89@hotmail.com>
2022-10-05 18:29:42 -0700

    done for the day

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// lets grab the elements we need first
+
+const model = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);

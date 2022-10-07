f60c6280ab23e8d5f2fef1b0de848f0f7d800697
Anthony Campos <Anthonyck89@hotmail.com>
2022-10-06 18:32:19 -0700

    functionality add

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -2,9 +2,25 @@
 
 // lets grab the elements we need first
 
-const model = document.querySelector('.model');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  // use classList to remove a class modifier such as hidden
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+} // end for
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);

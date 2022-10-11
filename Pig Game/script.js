820e491010d9bb4e5d4dee9f8e43c742e5a60fdd
Anthony Campos <Anthonyck89@hotmail.com>
2022-10-10 17:57:11 -0700

    player objects created

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// game objects
+
+/** Game Player Objects  */
+const playerZero = {
+  totalScoreElement: document.getElementById('score--0'),
+  totalScore: 0,
+  currentScoreElement: document.getElementById('current--0'),
+  currentScore: 0,
+}; // playerZero object
+
+const playerOne = {
+  totalScoreElement: document.getElementById('score--1'),
+  totalScore: 0,
+  currentScoreElement: document.getElementById('current--1'),
+  currentSCore: 0,
+}; // playerOne Object
+
+/** Initial Game Loading Up */
+resetGame();
+
+/** Game Fuctions  */
+
+function resetGame() {
+  resetScore(playerZero.totalScore, playerZero.totalScoreElement);
+  resetScore(playerOne.totalScore, playerOne.totalScoreElement);
+} // end of resetGame
+
+function resetScore(score, element) {
+  score = 0;
+  updateText(element, score);
+} // end of resetScore
+
+function updateText(element, text) {
+  element.textContent = text;
+} // end updateText

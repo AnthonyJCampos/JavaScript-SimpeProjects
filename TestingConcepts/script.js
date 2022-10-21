6ba42dfa481065f9f5fd071f4730227706843308
Anthony Campos <Anthonyck89@hotmail.com>
2022-10-21 12:52:01 -0700

    added code

--- TestingConcepts/script.js
+++ TestingConcepts/script.js
@@ -25,5 +25,46 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     },
+  }, // end opening hours
+
+  /** Methods  */
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-};
+}; // end restaurant
+
+/** Array Destructoring ES6 */
+
+// away of unpacking values into variables
+
+const arr1 = [1, 2, 3];
+
+// destructoring;
+
+// example of an array being destructed into variables
+const [x, y, z] = arr1;
+
+// testing through console output
+console.log(x, y, z);
+
+//grab the first two elements, note variable name has no impact
+let [first, second] = restaurant.categories;
+console.log(first, second);
+
+// example of skipping elements
+console.log('Skipping Elements with Array Destructoring');
+let [index1, , index3] = restaurant.categories;
+console.log(index1, index3);
+
+// you can use array destructing to swap elements
+console.log('Swapping with Array Destructoring');
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Array Destructoring can be used to recieve an array from a method
+
+let [starter, mainCourse] = restaurant.order(2, 2);
+
+console.log(starter, mainCourse);

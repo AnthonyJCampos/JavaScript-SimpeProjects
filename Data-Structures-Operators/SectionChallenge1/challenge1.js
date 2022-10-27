759ffb92325cfe8be9379bf1c739824c02d51466
Anthony Campos <Anthonyck89@hotmail.com>
2022-10-26 18:03:34 -0700

    challenge 1 complete

--- Data-Structures-Operators/SectionChallenge1/challenge1.js
+++ Data-Structures-Operators/SectionChallenge1/challenge1.js
@@ -20,3 +20,90 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 
 GOOD LUCK 😀
 */
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ], // end players
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  }, // end odds
+}; // end game
+
+//1. Create one player array for each team (variables 'players1' and 'players2')
+
+const [players1, players2] = game.players; // use object deconstructing
+console.log(players1);
+console.log(players2);
+
+//2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+
+const [gk, ...fieldPlayers] = players1; // use object deconstructing again
+
+console.log(gk, fieldPlayers);
+
+//3. Create an array 'allPlayers' containing all players of both teams (22 players)
+
+const allPlayers = [...players1, ...players2]; // using the Spread Operator
+console.log(allPlayers);
+
+//4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+//5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+
+const { team1, x: draw, team2 } = game.odds; // more deconstructing
+
+console.log(team1);
+console.log(draw);
+console.log(team2);
+
+//6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+printNames(players1);
+
+function printNames(...names) {
+  console.log('Players: ');
+  console.log(...names);
+  console.log(`Goals: ${names.length}`);
+}
+
+//7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+const winningTeam = (team1 < team2 && game.team1) || game.team2;
+
+console.log(winningTeam);

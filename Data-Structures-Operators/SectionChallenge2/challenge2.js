533155b767d69e52df85c0745870bbda8cf15601
Anthony Campos <Anthonyck89@hotmail.com>
2022-10-29 18:02:58 -0700

    test

--- Data-Structures-Operators/SectionChallenge2/challenge2.js
+++ Data-Structures-Operators/SectionChallenge2/challenge2.js
@@ -113,6 +113,7 @@ for (const [key, odd] of teamOdds) {
   const teamName = ` ${game?.[key] ?? ''}`;
   console.log(`Odd of ${text}${teamName} ${odd}`);
 } // end for
+
 //BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
 //      {
 //        Gnarby: 1,

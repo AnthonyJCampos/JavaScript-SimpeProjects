55147fd6508140127bcc174c60140978c6305cee
Anthony Campos <Anthonyck89@hotmail.com>
2022-11-14 08:45:12 -0800

    filter method

--- Arrays/TestingConcepts/script2.js
+++ Arrays/TestingConcepts/script2.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/** ARRAY MAP METHODS */
+/** ARRAY MAP METHOD */
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -11,6 +11,8 @@ const movementsUSD = movements.map(mov => Math.trunc(mov * eurToUSD));
 console.log(movements);
 console.log(movementsUSD);
 
+// The map() method creates a new array populated with the results of calling a
+// provided function on every element in the calling array.
 const stringArr = movements.map(function (mov, index) {
   return `Movement ${
     index + 1
@@ -18,3 +20,17 @@ const stringArr = movements.map(function (mov, index) {
 });
 
 stringArr.forEach(str => console.log(str));
+
+/** ARRAY FILTER METHOD */
+// The filter() method creates a shallow copy of a portion of a given array,
+// filtered down to just the elements from the given array that pass the test
+// implemented by the provided function.
+
+// this will create a new array of all postive values
+const deposits = movements.filter(mov => mov > 0);
+
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);

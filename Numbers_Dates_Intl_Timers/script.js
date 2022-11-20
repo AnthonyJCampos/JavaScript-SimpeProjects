5338c61415a546f05b5581b17428acc5d6f5aad7
Anthony Campos <Anthonyck89@hotmail.com>
2022-11-19 18:54:56 -0800

    date operations

--- Numbers_Dates_Intl_Timers/script.js
+++ Numbers_Dates_Intl_Timers/script.js
@@ -132,6 +132,15 @@ console.log(future.getSeconds());
 console.log(future.toISOString());
 console.log(future.getTime());
 
-console.log(new Date(2142285780000));
-
+// get current time stamp
 console.log(Date.now());
+
+/** Operations with Dates */
+
+console.log(Number(future));
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs((date2 - date1) / (1000 * 60 * 60 * 24));
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
